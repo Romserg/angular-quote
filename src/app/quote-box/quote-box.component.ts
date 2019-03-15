cedee0e9bb4843054f806e9920593bebb67237e5
Romserg <serg.romankov@gmail.com>
2019-03-15 14:48:01 +0200

    Add possibility to get new quote

--- src/app/quote-box/quote-box.component.ts
+++ src/app/quote-box/quote-box.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GetQuoteService } from '../services/get-quote.service';
-import { Quote } from '../models/quote';
 
 @Component({
   selector: 'app-quote-box',
@@ -8,14 +7,22 @@ import { Quote } from '../models/quote';
   styleUrls: ['./quote-box.component.css']
 })
 export class QuoteBoxComponent implements OnInit {
-  public  quote = new Quote('','');
+  public  quote;
+  public  author;
   constructor(public quoteService: GetQuoteService) {
-    this.quoteService.getQuotes().subscribe((data:any[])=> {
-      console.log(data);
-    })
-  }
 
+  }
+  public getQuotes = () => {
+    this.quoteService.getQuotes().subscribe((data:any)=> {
+        let quotes = data.quotes;
+        let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        this.quote =  randomQuote.quote;
+        this.author = randomQuote.author;
+      },
+      err => console.log(err))
+  };
   ngOnInit() {
+    this.getQuotes()
   }
 
 }
